e07155ee3864d4739033d1acc14363ed607b96c5
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-12-03 00:53:26 +0000

    Criando as Migrations e dando Update para criação das tabelas do banco de dados.

--- TodoApp/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApp/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77095b25d6077d50a82e2a9f74e28eb4e09179f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eaf580cf9041d6ca8305e55ab0b2157b245f92c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
