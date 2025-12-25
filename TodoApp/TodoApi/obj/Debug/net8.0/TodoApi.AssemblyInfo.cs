889df45f2c195a7eabe5d8cb4d50ba4ef8180f4a
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-12-25 22:33:50 +0000

    Subindo alterações no codigo.

--- TodoApp/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApp/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ffc0c353a0412e249d58d9a950d75200844285")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372351ceb0285a46174cacb19cbb0e4f8f8c7f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
