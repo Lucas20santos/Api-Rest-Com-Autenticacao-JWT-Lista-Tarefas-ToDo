b0ffc0c353a0412e249d58d9a950d75200844285
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-12-25 21:11:02 +0000

    Correções...

--- TodoApp/TodoApi/Helpers/JwtService.cs
+++ TodoApp/TodoApi/Helpers/JwtService.cs
@@ -1,8 +1,8 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
 using TodoApi.Models.Domain;
 using TodoApi.Services.Interfaces;
 
@@ -18,7 +18,7 @@ namespace TodoApi.Helpers
         public string GenerateToken(User user)
         {
             var jwt = _config.GetSection("JwtSettings");
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Secret"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Secret"]!));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var claims = new List<Claim>
@@ -26,7 +26,8 @@ namespace TodoApi.Helpers
                 new Claim(JwtRegisteredClaimNames.Sub, user.Username),
                 new Claim("uid", user.Id.ToString())
             };
-            var token = new JwtSecurityToken(
+            var token = new JwtSecurityToken
+            (
                 issuer: jwt["Issuer"],
                 audience: jwt["Audience"],
                 claims: claims,
